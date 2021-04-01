586470479c893dade4b4f1dd874d74fd298d0fcc
Nitoriubar <81606992+Nitoriubar@users.noreply.github.com>
2021-04-01 15:49:44 +0900

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -17,24 +17,24 @@ public class diamond {
 			{
 				System.out.print("*");
 			}
-			System.out.println();				  //삼각형을 만들기 위해 개행
+			System.out.println();				//삼각형을 만들기 위해 개행
 		}
 		
 		
 		
 		for(int i=1;i<=num;i++) 				//마름모의 중간의 가장 긴부분을 위해 시작을 1부터 num까지 도는 행변수 i 
 		{
-			for(int j=1;j<=i;j++)				  //공백을 찍기 위한 열변수 j, 공백이 행이 늘어갈수록 같이 늘어나기 때문에 i까지 돌림 
+			for(int j=1;j<=i;j++)				//공백을 찍기 위한 열변수 j, 공백이 행이 늘어갈수록 같이 늘어나기 때문에 i까지 돌림 
 			{
 				System.out.print(" ");
 			}
-			for(int k=(i*2)-1;k<=(num*2)-3;k++)	  //*을 찍기 위한 열변수 k, 가장긴부분부터 시작하고 양쪽하나씩 줄어들어야하기 때문에 (i*2)-1부터(num*2)-3까지 돌림
+			for(int k=(i*2)-1;k<=(num*2)-3;k++)	  	//*을 찍기 위한 열변수 k, 가장긴부분부터 시작하고 양쪽하나씩 줄어들어야하기 때문에 (i*2)-1부터(num*2)-3까지 돌림
 			{
 				System.out.print("*");
 			}
-				System.out.println();			  //역삼각형을 만들기 위해 개행
+				System.out.println();			//역삼각형을 만들기 위해 개행
 		}
-		scan.close();							      //스캐너 닫기
+		scan.close();						//스캐너 닫기
 	}
 	
 
